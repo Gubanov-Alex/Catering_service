19f61bf9cf8adda7b10479c97d8bba5d3276058e
Gubanov-Alex <future.htm@gmail.com>
2025-02-06 15:45:27 +0200

    Remove `orders` app and restructure models into existing apps
    The `orders` app has been removed, and its models have been restructured and integrated into the `users` and `restaurants` apps. Additionally, typo fixes have been applied across the `config` project. These changes simplify the codebase by reducing duplication and eliminating the need for a separate app for orders.

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from users.models import Order
+
+PROVIDER_CHOICES = (
+    ('Uber', 'Uber'),
+    ('Uklon', 'Uklon'),
+    # etc.
+)
+STATUS_CHOICES = (
+    ('ready', 'Ready'),
+    ('finished', 'Finished'),
+    ('in_progress', 'In Progress'),
+    # etc.
+)
+
 
 class Restaurant(models.Model):
     """ Restaurant model"""
@@ -20,3 +34,41 @@ class Dish(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class DishOrder(models.Model):
+    """
+     Intermediate model representing an ordered dish and its quantity.
+    """
+    dish_order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='order_dishes'
+    )
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.dish.name}"
+
+
+class DeliveryOrder(models.Model):
+    """
+    Logs delivery details for internal or external services.
+    """
+
+    provider = models.CharField(max_length=50, choices=PROVIDER_CHOICES)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='in_progress')
+    addresses = models.TextField()
+    external_order_id = models.CharField(max_length=100, unique=True)
+
+    dish_order = models.ForeignKey(
+        DishOrder,
+        on_delete=models.CASCADE,
+        related_name='delivery'
+    )
+
+
+    def __str__(self):
+        return f"Delivery {self.id} [{self.provider}]"
+
--- users/models.py
+++ users/models.py
@@ -11,3 +11,20 @@ class User(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.phone})"
+
+
+class Order(models.Model):
+    """
+    This might represent one "order" of dishes from the user,
+    sometimes called the 'cart' or 'basket' before finalization.
+    """
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='dish_orders'
+    )
+    external_order_id = models.CharField(max_length=100,unique=True)
+
+    def __str__(self):
+        return f"DishOrder {self.id} by {self.user}"
+
